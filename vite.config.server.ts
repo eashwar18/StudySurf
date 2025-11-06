6f7f4f1670b4bb1a52e73e45490faaae42d0b543
Eashwar <eash.kshankar@gmail.com>
2025-11-06 21:27:20 +0530

    alias change

--- vite.config.server.ts
+++ vite.config.server.ts
@@ -1,16 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 import { fileURLToPath, URL } from 'url';
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from 'path';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   build: {
     outDir: "dist/spa",
   },
   plugins: [react()],
   resolve: {
     alias: {
-      'a': path.resolve(fileURLToPath(new URL('.', import.meta.url)), 'client')
-    },
-  },
-}));
+      // This one fixes your build error
+      'a': path.resolve(fileURLToPath(new URL('.', import.meta.url)), 'client'),
+
+      // This one matches the @shared from your tsconfig
+      '@shared': path.resolve(fileURLToPath(new URL('.', import.meta.url)), 'shared')
+    }
+  }
+})
