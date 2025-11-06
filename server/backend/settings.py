9f2a9535d3b6ae880633bed20dc5975ef125316d
Eashwar <eash.kshankar@gmail.com>
2025-11-06 20:30:24 +0530

    1.1 fix

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -26,8 +26,6 @@ SECRET_KEY = 'django-insecure-x#-9y2s@o&fu!kwm+bm@v@rq+a!+jube-o1o*hcj$_a@-rjkad
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
