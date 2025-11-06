e19b797fe6dc6e4f399a6a036db3e3da87b264c4
Eashwar <eash.kshankar@gmail.com>
2025-11-06 23:18:15 +0530

    frontend alias

--- vite.config.server.ts
+++ vite.config.server.ts
@@ -1,19 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } in 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
-  build: {
-    outDir: "dist/spa",
-  },
-  plugins: [react()],
+  //...
   resolve: {
     alias: {
-      // This one matches your code
       '@': path.resolve(fileURLToPath(new URL('.', import.meta.url)), 'client'),
-
-      // This one matches the @shared from your tsconfig
       '@shared': path.resolve(fileURLToPath(new URL('.', import.meta.url)), 'shared')
     }
   }
